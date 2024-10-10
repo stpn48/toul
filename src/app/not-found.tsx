a870c271e18eefff691b3e40518e17194d1ace30
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-10 19:02:54 +0200

    Create not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFondPage() {
+  return (
+    <div className="flex w-full items-center justify-center text-black dark:text-white">
+      This page doesn&apos;t exist
+    </div>
+  );
+}
