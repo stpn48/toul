181d87fa62c970be9154db2190579fe244b3f62d
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-10 19:03:00 +0200

    Create use-sidebar-visibility.tsx

---
+++ src/components/Sidebar/use-sidebar-visibility.tsx
@@ -0,0 +1,14 @@
+import { stat } from "fs";
+import { create } from "zustand";
+
+interface Store {
+  showSidebar: boolean;
+  setShowSidebar: (showSidebar: boolean | ((prev: boolean) => boolean)) => void;
+}
+export const useSidebarVisibility = create<Store>((set) => ({
+  showSidebar: true,
+  setShowSidebar: (showSidebar) =>
+    set((state) => ({
+      showSidebar: typeof showSidebar === "function" ? showSidebar(state.showSidebar) : showSidebar,
+    })),
+}));
