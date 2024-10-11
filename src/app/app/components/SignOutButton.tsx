39af92fb79573c25989422b999fb5fabf41888a6
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-11 18:37:37 +0200

    Create SignOutButton.tsx

---
+++ src/app/app/components/SignOutButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { signOut } from "@/app/actions/supabaseAuth";
+import React, { useCallback, useTransition } from "react";
+import toast from "react-hot-toast";
+
+export default function SignOutButton() {
+  const [isSigningOut, startSigningOut] = useTransition();
+
+  const handleSignOut = useCallback(() => {
+    startSigningOut(async () => {
+      const { error } = await signOut();
+      if (error) {
+        toast.error(error);
+        return;
+      }
+    });
+  }, []);
+
+  return (
+    <button disabled={isSigningOut} onClick={handleSignOut} className="absolute bottom-4 right-4">
+      Sign out
+    </button>
+  );
+}
