2321c0b8582bf19c1793367dc445d9dd4b6fa31e
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-12 22:54:20 +0200

    Update WorkoutList.tsx

--- src/app/app/your-workouts/components/WorkoutList.tsx
+++ src/app/app/your-workouts/components/WorkoutList.tsx
@@ -2,14 +2,18 @@
 
 import { useOptimisticWorkouts } from "@/context/useOptimisticWorkouts";
 import React from "react";
+import { WorkoutCard } from "./WorkoutCard";
 
 export function WorkoutList() {
   const { optimisticWorkouts } = useOptimisticWorkouts();
 
   return (
-    <div>
+    <div className="flex w-full flex-wrap justify-center gap-4">
+      {optimisticWorkouts.length === 0 && (
+        <p className="text-center text-xs text-secondary">No workouts yet...</p>
+      )}
       {optimisticWorkouts.map((workout) => (
-        <div key={workout.id}>{workout.name}</div>
+        <WorkoutCard key={workout.id} workout={workout} />
       ))}
     </div>
   );
