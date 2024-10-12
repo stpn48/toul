374a693415283b25af8e4b6a809bd349e1457d62
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-12 22:54:09 +0200

    Create updateExercise.ts

---
+++ src/app/actions/updateExercise.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { CreateExercise } from "@/types/types";
+import prisma from "@/utils/prisma";
+import { getUser } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+
+export async function updateExercise(newExercise: CreateExercise, exerciseId: string) {
+  const user = await getUser();
+
+  if (!user) {
+    return { error: "You must be logged in to update an exercise." };
+  }
+
+  if (!newExercise.name || newExercise.sets.length === 0) {
+    return { error: "Please fill in all fields" };
+  }
+
+  if (!exerciseId) {
+    return { error: "Please provide an exerciseId" };
+  }
+
+  try {
+    await prisma.exercise.update({
+      where: {
+        id: exerciseId,
+      },
+      data: {
+        name: newExercise.name,
+        description: newExercise.description,
+        sets: {
+          deleteMany: {},
+          create: newExercise.sets.map((set) => ({
+            name: set.name,
+            targetReps: set.targetReps,
+          })),
+        },
+      },
+    });
+  } catch (error) {
+    return { error: "Error updating exercise" }; //TODO: Add better error handling
+  }
+
+  revalidatePath("/app");
+
+  return { error: null };
+}
