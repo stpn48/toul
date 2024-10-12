3d7422f58e4a3c0194b3eae80ba144314f48e773
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-12 22:54:12 +0200

    Update EditExerciseModal.tsx

--- src/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/EditExerciseModal.tsx/EditExerciseModal.tsx
+++ src/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/EditExerciseModal.tsx/EditExerciseModal.tsx
@@ -1,80 +1,117 @@
 "use client";
 
-import { addExercise } from "@/app/actions/addExercise";
+import { getExerciseDetails } from "@/app/actions/getExerciseDetails";
+import { updateExercise } from "@/app/actions/updateExercise";
+import { AddSetModal } from "@/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/CreateExerciseModal/AddSetModal";
+import CreateExerciseModalHeader from "@/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/CreateExerciseModal/CreateExerciseModalHeader";
+import { SetList } from "@/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/CreateExerciseModal/SetList";
 import Input from "@/components/Input";
 import { Modal } from "@/components/Modal";
 import { useOptimisticWorkouts } from "@/context/useOptimisticWorkouts";
 import { useModalVisibility } from "@/store/useModalVisiblity";
 import { CreateExercise, CreateSet, ExerciseWithSets } from "@/types/types";
-import { useCallback, useState } from "react";
+import prisma from "@/utils/prisma";
+import { useQuery } from "@tanstack/react-query";
+import { setServers } from "dns";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { v4 as uuidv4 } from "uuid";
-import { AddSetModal } from "../CreateExerciseModal/AddSetModal";
-import { CreateExerciseButton } from "../CreateExerciseModal/CreateExerciseButton";
-import CreateExerciseModalHeader from "../CreateExerciseModal/CreateExerciseModalHeader";
-import { SetList } from "../CreateExerciseModal/SetList";
+import { EditExerciseButton } from "./EditExerciseButton";
 
 type Props = {
-  workoutId: string;
+  exerciseId: string | null;
 };
 
-export function EditExerciseModal({ workoutId }: Props) {
-  const { setShowCreateExerciseModal, showCreateSetModal } = useModalVisibility();
-  const { addExerciseOptimistic } = useOptimisticWorkouts();
+export function EditExerciseModal({ exerciseId }: Props) {
+  const { showCreateSetModal, setEditingExerciseId } = useModalVisibility();
+  const { updateOptimisticExercise } = useOptimisticWorkouts();
+  // TODO: move ths query into separate hook
+  const {
+    data: exercise,
+    isLoading,
+    isSuccess,
+  } = useQuery({
+    queryKey: ["exercise", { exerciseId }],
+    queryFn: () => getExerciseDetails(exerciseId || ""),
+  });
+
+  const router = useRouter();
 
   const [exerciseName, setExerciseName] = useState<string | null>(null);
   const [exerciseDescription, setExerciseDescription] = useState<string | null>(null);
   const [exerciseSets, setExerciseSets] = useState<CreateSet[]>([]);
 
-  const handleCreateExercise = useCallback(async () => {
+  const handleEditExercise = useCallback(async () => {
+    if (!exercise) {
+      return;
+    }
     // validate fields
     if (!exerciseName || exerciseSets.length === 0) {
       toast.error("Please fill in all fields");
       return;
     }
 
-    // add exercise optimistically
+    const updatedExercise: CreateExercise = {
+      name: exerciseName,
+      description: exerciseDescription,
+      sets: exerciseSets,
+    };
+
     const optimisticExercise: ExerciseWithSets = {
-      workoutId,
-      userId: "1",
-      sets: exerciseSets,
-      id: uuidv4(),
-      createdAt: new Date(),
-      repHistory: [],
-      timesCompleted: 0,
+      workoutId: exercise.workoutId,
+      userId: exercise.userId,
+      sets: exerciseSets.map((set) => ({
+        name: set.name,
+        targetReps: set.targetReps,
+        exerciseId: exercise.id,
+        id: uuidv4(),
+        createdAt: new Date(),
+      })),
+      id: exercise.id,
+      createdAt: exercise.createdAt,
+      repHistory: exercise.repHistory,
+      timesCompleted: exercise.timesCompleted,
       description: exerciseDescription,
       name: exerciseName,
     };
 
-    addExerciseOptimistic(optimisticExercise);
+    updateOptimisticExercise(optimisticExercise, exercise.id);
 
-    // close modal after adding exercise optimistically
-    setShowCreateExerciseModal(false);
-
-    // reset state
-    setExerciseName(null);
-    setExerciseDescription(null);
-    setExerciseSets([]);
-
-    const newExercise: CreateExercise = {
-      name: exerciseName,
-      description: exerciseDescription,
-      sets: exerciseSets,
-    };
+    setEditingExerciseId(null);
 
     // add exercise to workout in db
-    const { error } = await addExercise(newExercise, workoutId);
+    const { error } = await updateExercise(updatedExercise, exercise.id);
 
     // if error show error toast
     if (error) {
       toast.error(error);
+
+      // no error, reset state
+    } else {
+      setExerciseName("");
+      setExerciseDescription(null);
+      setExerciseSets([]);
     }
-  }, [exerciseName, exerciseSets, exerciseDescription]);
+  }, [exerciseName, exerciseSets, exerciseDescription, exercise, updateOptimisticExercise]);
+
+  useEffect(() => {
+    if (!isSuccess) {
+      return;
+    }
+    setExerciseName(exercise?.name || "");
+    setExerciseDescription(exercise?.description || "");
+    setExerciseSets(exercise?.sets || []);
+  }, [isSuccess, exercise]);
+
+  if (isLoading) {
+    return <div>loading...</div>; //TODO: Add skeleton
+  }
 
   return (
     <Modal
       className="flex h-[70%] w-[70%] flex-row text-sm"
-      closeModal={() => setShowCreateExerciseModal(false)}
+      closeModal={() => setEditingExerciseId(null)}
     >
       <section className="w-[30%] rounded-l-lg bg-[#faf8f6] p-6">
         <Input
@@ -98,7 +135,7 @@ export function EditExerciseModal({ workoutId }: Props) {
       <section className="flex-grow p-4">
         <CreateExerciseModalHeader />
         <SetList exerciseSets={exerciseSets} />
-        <CreateExerciseButton handleCreateExercise={handleCreateExercise} />
+        <EditExerciseButton handleEditExercise={handleEditExercise} />
       </section>
 
       {showCreateSetModal && <AddSetModal setExerciseSets={setExerciseSets} />}
