3462deacaee82b89bf3372eddfedc540edac5b49
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-13 18:30:28 +0200

    Update ExercisesSectionHeader.tsx

--- src/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/ExercisesSectionHeader.tsx
+++ src/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/ExercisesSectionHeader.tsx
@@ -3,15 +3,18 @@
 import { useModalVisibility } from "@/store/useModalVisiblity";
 import React from "react";
 
-type Props = {};
+type Props = {
+  loading?: boolean;
+};
 
-export function ExercisesSectionHeader({}: Props) {
+export function ExercisesSectionHeader({ loading }: Props) {
   const { setShowCreateExerciseModal } = useModalVisibility();
 
   return (
     <div className="flex items-center justify-between pr-5">
       <h1 className="text-xs font-bold text-secondary">EXERCISES</h1>
       <button
+        disabled={loading}
         onClick={() => setShowCreateExerciseModal(true)}
         className="flex items-center gap-2 rounded-full bg-amber-500 px-2 py-1 text-white"
       >
@@ -24,12 +27,7 @@ export function ExercisesSectionHeader({}: Props) {
 
 function PlusIcon() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="size-4"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
       <path
         fillRule="evenodd"
         d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
