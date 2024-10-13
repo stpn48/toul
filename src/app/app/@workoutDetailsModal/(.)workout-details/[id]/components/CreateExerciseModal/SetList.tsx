f0374976ebf9033f41994f995f44d362b1e56372
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-13 18:30:23 +0200

    Update SetList.tsx

--- src/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/CreateExerciseModal/SetList.tsx
+++ src/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/CreateExerciseModal/SetList.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import { CreateSet } from "@/types/types";
-import React from "react";
+import React, { SetStateAction } from "react";
 import { SetCard } from "./SetCard";
 
 type Props = {
   exerciseSets: CreateSet[];
+  setEditingSetIndex: React.Dispatch<SetStateAction<number | null>>;
 };
 
-export function SetList({ exerciseSets }: Props) {
+export function SetList({ exerciseSets, setEditingSetIndex }: Props) {
   return (
     <div className="mt-10 flex flex-col gap-4">
-      {exerciseSets.length === 0 && (
-        <p className="flex w-full justify-center text-xs text-secondary">No sets yet...</p>
-      )}
+      {exerciseSets.length === 0 && <p className="flex w-full justify-center text-xs text-secondary">No sets yet...</p>}
       {exerciseSets.map((set, index) => (
-        <SetCard key={index} set={set} />
+        <SetCard onClick={() => setEditingSetIndex(index)} key={index} set={set} />
       ))}
     </div>
   );
