fb963459c7e68ed4f551675c9fb67902842449ae
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-13 18:30:18 +0200

    Update getExerciseDetails.ts

--- src/app/actions/getExerciseDetails.ts
+++ src/app/actions/getExerciseDetails.ts
@@ -1,20 +1,32 @@
 "use server";
 
 import prisma from "@/utils/prisma";
+import { getUser } from "@/utils/supabase/server";
 
 export async function getExerciseDetails(exerciseId: string) {
-  //TODO: Add security and error handling
+  const user = await getUser();
 
-  if (!exerciseId) {
-    return null;
+  if (!user) {
+    throw new Error("You must be logged in to get exercise details");
   }
 
-  return await prisma.exercise.findFirst({
-    where: {
-      id: exerciseId,
-    },
-    include: {
-      sets: true,
-    },
-  });
+  if (!exerciseId) {
+    throw new Error("Please provide an exerciseId");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 10));
+
+  try {
+    const exercises = await prisma.exercise.findFirst({
+      where: {
+        id: exerciseId,
+      },
+      include: {
+        sets: true,
+      },
+    });
+    return exercises;
+  } catch (error) {
+    throw new Error("Error getting exercise details");
+  }
 }
