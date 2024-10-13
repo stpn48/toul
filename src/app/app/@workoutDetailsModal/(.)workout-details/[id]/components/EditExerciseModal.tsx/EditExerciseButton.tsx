9becce6e4d3cceae622f54412dbeb5500c1bf60c
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-13 18:30:23 +0200

    Update EditExerciseButton.tsx

--- src/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/EditExerciseModal.tsx/EditExerciseButton.tsx
+++ src/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/EditExerciseModal.tsx/EditExerciseButton.tsx
@@ -8,7 +8,7 @@ export function EditExerciseButton({ handleEditExercise }: Props) {
   return (
     <button
       onClick={() => handleEditExercise()}
-      className="absolute bottom-4 right-4 flex w-fit justify-center rounded-full bg-amber-500 px-4 py-2 text-white"
+      className="flex w-fit justify-center rounded-full bg-amber-500 px-4 py-2 text-white"
     >
       Edit Exercise
     </button>
