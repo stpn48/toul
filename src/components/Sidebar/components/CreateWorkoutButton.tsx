cf22f65e1d97a5ab3c6a96ffcb769910ec2d7beb
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-14 18:58:24 +0200

    Update CreateWorkoutButton.tsx

--- src/components/Sidebar/components/CreateWorkoutButton.tsx
+++ src/components/Sidebar/components/CreateWorkoutButton.tsx
@@ -9,14 +9,9 @@ export default function CreateWorkoutButton() {
   return (
     <button
       onClick={() => setShowCreateWorkoutModal(true)}
-      className="flex items-center gap-2 hover:text-[#454545]"
+      className="text-main dark:text-dark-main hover:bg-hover dark:hover:bg-dark-hover flex w-full items-center gap-2 rounded-lg p-2"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        className="size-5"
-      >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
         <path
           fillRule="evenodd"
           d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
