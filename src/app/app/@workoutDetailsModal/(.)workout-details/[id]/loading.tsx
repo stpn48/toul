a69fb70c8374671807627c67860f4b3140be0e29
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-14 18:58:18 +0200

    Update loading.tsx

--- src/app/app/@workoutDetailsModal/(.)workout-details/[id]/loading.tsx
+++ src/app/app/@workoutDetailsModal/(.)workout-details/[id]/loading.tsx
@@ -8,11 +8,11 @@ import { ExercisesSectionHeader } from "./components/ExercisesSectionHeader";
 export default function Loading() {
   return (
     <Modal className="flex flex-row" closeModal={() => {}}>
-      <section className="flex w-[30%] flex-col gap-6 rounded-l-lg bg-[#faf8f6] p-6">
-        <div className="h-[60px] w-full animate-pulse rounded-lg bg-stone-300" />
-        <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-300" />
-        <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-300" />
-        <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-300" />
+      <section className="bg-secondary dark:bg-dark-secondary flex w-[30%] flex-col gap-6 rounded-l-lg p-6">
+        <div className="h-[60px] w-full animate-pulse rounded-lg bg-stone-300 dark:bg-stone-900" />
+        <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-300 dark:bg-stone-900" />
+        <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-300 dark:bg-stone-900" />
+        <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-300 dark:bg-stone-900" />
       </section>
       <section className="relative h-full flex-grow rounded-r-lg p-4">
         <ExercisesSectionHeader loading={true} />
