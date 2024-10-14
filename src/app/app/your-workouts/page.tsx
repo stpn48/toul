34e1614344d9fd953599dae937f6cdb243ece9a0
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-14 18:58:20 +0200

    Update page.tsx

--- src/app/app/your-workouts/page.tsx
+++ src/app/app/your-workouts/page.tsx
@@ -3,7 +3,7 @@ import { WorkoutList } from "./components/WorkoutList";
 
 export default async function YourWorkoutsPage() {
   return (
-    <div className="flex h-full w-full flex-grow flex-col gap-8 text-black dark:text-white">
+    <div className="text-main dark:text-dark-main flex h-full w-full flex-grow flex-col gap-8">
       <h1 className="text-2xl font-bold">Your Workouts</h1>
       <WorkoutList />
     </div>
