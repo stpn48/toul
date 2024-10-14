50f819e0890895d3fe0d10ff701cbd68661780d3
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-14 06:29:24 +0200

    Update loading.tsx

--- src/app/app/@workoutDetailsModal/(.)workout-details/[id]/loading.tsx
+++ src/app/app/@workoutDetailsModal/(.)workout-details/[id]/loading.tsx
@@ -9,10 +9,10 @@ export default function Loading() {
   return (
     <Modal className="flex flex-row" closeModal={() => {}}>
       <section className="flex w-[30%] flex-col gap-6 rounded-l-lg bg-[#faf8f6] p-6">
-        <div className="h-[60px] w-full animate-pulse rounded-lg bg-stone-400" />
-        <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-400" />
-        <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-400" />
-        <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-400" />
+        <div className="h-[60px] w-full animate-pulse rounded-lg bg-stone-300" />
+        <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-300" />
+        <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-300" />
+        <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-300" />
       </section>
       <section className="relative h-full flex-grow rounded-r-lg p-4">
         <ExercisesSectionHeader loading={true} />
