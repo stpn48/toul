fb84194e9e13542255b55f7d509866e34eb93c98
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-14 06:29:20 +0200

    Update SetCard.tsx

--- src/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/CreateExerciseModal/SetCard.tsx
+++ src/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/CreateExerciseModal/SetCard.tsx
@@ -11,11 +11,14 @@ type Props = {
 export function SetCard({ set, onClick }: Props) {
   return (
     <>
-      <div onClick={onClick} className="flex cursor-pointer justify-between bg-white px-4 text-xs font-bold text-secondary">
+      <div
+        onClick={onClick}
+        className="flex cursor-pointer justify-between rounded-lg bg-white px-4 py-2 text-xs font-bold text-secondary hover:bg-amber-500 hover:bg-opacity-20 dark:bg-inherit dark:bg-opacity-30 dark:hover:bg-stone-800"
+      >
         <h1>{set.name}</h1>
         <p>{set.targetReps} reps</p>
       </div>
-      <div className="flex h-px w-full bg-[#b8b8b8]"></div>
+      <div className="flex h-px w-full bg-[#b8b8b8] dark:bg-[#5c5c5c]"></div>
     </>
   );
 }
