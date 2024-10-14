8092da69e13a19bddeeb0f4d15e2c4e964cfb43e
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-14 06:29:29 +0200

    Update LoadingBalls.tsx

--- src/components/LoadingBalls.tsx
+++ src/components/LoadingBalls.tsx
@@ -4,19 +4,19 @@ export function LoadingBalls({}) {
   return (
     <div className="mt-10 flex w-full justify-center gap-1">
       <div
-        className="ball-animation h-2 w-2 animate-pulse rounded-full bg-stone-400"
+        className="ball-animation h-2 w-2 animate-pulse rounded-full bg-stone-300"
         style={{
           animationDelay: "0s",
         }}
       ></div>
       <div
-        className="ball-animation h-2 w-2 animate-pulse rounded-full bg-stone-400"
+        className="ball-animation h-2 w-2 animate-pulse rounded-full bg-stone-300"
         style={{
           animationDelay: "0.1s",
         }}
       ></div>
       <div
-        className="ball-animation h-2 w-2 animate-pulse rounded-full bg-stone-400"
+        className="ball-animation h-2 w-2 animate-pulse rounded-full bg-stone-300"
         style={{
           animationDelay: "0.2s",
         }}
