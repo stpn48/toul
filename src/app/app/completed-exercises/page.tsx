a10118e35407380dc503bcce40f161c737c7c0af
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-15 19:38:45 +0200

    Update page.tsx

--- src/app/app/completed-exercises/page.tsx
+++ src/app/app/completed-exercises/page.tsx
@@ -1,5 +1,39 @@
+import prisma from "@/utils/prisma";
+import { getUser } from "@/utils/supabase/server";
 import React from "react";
 
-export default function CompletedExercisesPage() {
-  return <div>CompletedExercises</div>;
+export default async function CompletedExercisesPage() {
+  const user = await getUser();
+
+  if (!user) {
+    return null;
+  }
+
+  const completedExercises = await prisma.exercise.findMany({
+    where: {
+      userId: user.id,
+      timesCompleted: {
+        gt: 0,
+      },
+    },
+    include: {
+      workout: true,
+    },
+  });
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Your Completed Exercises</h1>
+      <div>
+        {completedExercises.map((exercise) => (
+          <div key={exercise.id}>
+            <h2>{exercise.name}</h2>
+            <p>{exercise.description}</p>
+            <p>{exercise.timesCompleted} times completed</p>
+            <p>Workout: {exercise.workout.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
