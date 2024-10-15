7427f2f09bd7e0f3c0bc237e504a4e35d9084e39
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-15 19:38:46 +0200

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,15 +5,18 @@ type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
   className?: string;
   variant?: "primary" | "secondary" | "tertiary";
   children: React.ReactNode;
+  disabled?: boolean;
 };
 
-export default function Button({ className, variant, children, ...props }: Props) {
+export default function Button({ className, variant, children, disabled, ...props }: Props) {
   return (
     <button
+      disabled={disabled}
       className={twMerge(
         "rounded-lg bg-black px-4 py-2 text-sm text-white dark:bg-white dark:text-black",
         className,
         variant === "secondary" && "bg-stone-400",
+        disabled && "cursor-not-allowed opacity-50",
       )}
       {...props}
     >
