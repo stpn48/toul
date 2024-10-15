f882670271c5cbfe3c9437cbcefe96d1c7a900d3
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-15 06:41:23 +0200

    Update SettingsMenu.tsx

--- src/components/SettingsButton/SettingsMenu.tsx
+++ src/components/SettingsButton/SettingsMenu.tsx
@@ -1,37 +1,26 @@
 "use client";
 
 import SignOutButton from "@/app/app/components/SignOutButton";
-import { useTheme } from "next-themes";
-import Image from "next/image";
-import React, { useCallback } from "react";
+import React from "react";
+import { MenuButton } from "./MenuButton";
+import { ThemeToggleButton } from "./ThemeToggleButton";
 
 type Props = {
   closeMenu: () => void;
 };
 
-const MOON_ICON_URL = "/MoonIcon.svg";
-const SUN_ICON_URL = "/SunIcon.svg";
-
 export function SettingsMenu({ closeMenu }: Props) {
-  const { resolvedTheme, setTheme } = useTheme();
-
-  const handleChangeTheme = useCallback(() => {
-    setTheme(resolvedTheme === "dark" ? "light" : "dark");
-  }, [setTheme, resolvedTheme]);
-
-  //TODO: Improve design and add theme change fucntionality with icons
   return (
-    <div onClick={closeMenu} className="fixed inset-0 z-10 h-screen w-screen">
+    <>
+      <div onClick={closeMenu} className="fixed inset-0 z-10 h-screen w-screen"></div>
       <div
         onClick={(e) => e.stopPropagation()}
-        className="absolute right-4 top-8 z-50 flex flex-col gap-2 rounded-lg bg-[#faf8f6] p-2 shadow-lg dark:border dark:border-main dark:bg-dark dark:text-white"
+        className="z-50 flex flex-col gap-2 rounded-lg border border-main bg-secondary p-2 shadow-lg dark:border-dark-main dark:bg-dark-secondary dark:text-dark-main"
       >
-        <button className="flex items-center gap-2 whitespace-nowrap" onClick={handleChangeTheme}>
-          <p>Change Theme</p>
-          <Image src={resolvedTheme === "dark" ? MOON_ICON_URL : SUN_ICON_URL} alt="theme icon" width={20} height={20} />
-        </button>
+        <ThemeToggleButton />
+        <div className="h-px w-full bg-hover dark:bg-dark-hover" />
         <SignOutButton />
       </div>
-    </div>
+    </>
   );
 }
