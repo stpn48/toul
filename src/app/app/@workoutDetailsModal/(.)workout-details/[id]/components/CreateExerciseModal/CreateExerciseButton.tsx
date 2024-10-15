5689a3fa14de297da237cb44b10cc4a788da6d0e
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-15 06:41:17 +0200

    Update CreateExerciseButton.tsx

--- src/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/CreateExerciseModal/CreateExerciseButton.tsx
+++ src/app/app/@workoutDetailsModal/(.)workout-details/[id]/components/CreateExerciseModal/CreateExerciseButton.tsx
@@ -1,16 +1,14 @@
 "use client";
 
+import Button from "@/components/Button";
 import React from "react";
 
 type Props = { onClick: () => void };
 
 export function CreateExerciseButton({ onClick }: Props) {
   return (
-    <button
-      onClick={onClick}
-      className="absolute bottom-4 right-4 flex w-fit justify-center rounded-full bg-amber-500 px-4 py-2 text-white"
-    >
+    <Button onClick={onClick} className="absolute bottom-4 right-4">
       Create Exercise
-    </button>
+    </Button>
   );
 }
