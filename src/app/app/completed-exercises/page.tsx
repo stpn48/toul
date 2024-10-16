55dd6efcbba99752daf412ebc7cec4d5ce64e177
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-16 22:14:55 +0200

    Update page.tsx

--- src/app/app/completed-exercises/page.tsx
+++ src/app/app/completed-exercises/page.tsx
@@ -25,8 +25,8 @@ export default async function CompletedExercisesPage() {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold">Your Completed Exercises</h1>
-      <div className="mt-10 flex flex-wrap gap-4">
+      <h1 className="flex w-full justify-center text-2xl font-bold">your completed exercises</h1>
+      <div className="mt-10 flex flex-wrap justify-center gap-4">
         {completedExercises.length === 0 && (
           <p className="flex w-full justify-center text-center text-xs text-secondary">No completed exercises yet...</p>
         )}
