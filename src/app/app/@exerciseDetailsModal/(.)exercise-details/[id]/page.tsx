31bba67a4534c0063727ac5cc1506ee1c0db1b4a
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-16 06:37:00 +0200

    Create page.tsx

---
+++ src/app/app/@exerciseDetailsModal/(.)exercise-details/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function ExerciseDetailsModal({ params }: Props) {
+  //TODO: Show graph
+
+  return <h1>Exercise Details</h1>;
+}
