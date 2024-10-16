3f976ee4cbe90f8d60f5b807768ffec6f1ab4c65
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-16 22:14:58 +0200

    Update WorkoutCard.tsx

--- src/app/app/your-workouts/components/WorkoutCard.tsx
+++ src/app/app/your-workouts/components/WorkoutCard.tsx
@@ -13,13 +13,13 @@ export function WorkoutCard({ workout }: Props) {
       <div
         tabIndex={0}
         role="button"
-        className="flex w-full cursor-pointer justify-between gap-2 rounded-lg border border-main p-4 outline-none hover:border-hover dark:border-dark-main dark:hover:border-dark-hover"
+        className="flex h-[120px] w-[250px] cursor-pointer flex-col justify-between gap-4 rounded-lg border border-main p-4 outline-none transition-all hover:border-hover dark:border-dark-main dark:hover:border-dark-hover"
       >
         <div className="flex flex-col">
           <h1 className="max-w-[150px] truncate">{workout.name}</h1>
           <p className="max-w-[150px] truncate text-secondary">{workout.description}</p>
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex justify-between gap-2">
           <p className="text-red-600">{workout.difficulty}</p>
           <p>{workout.estimatedDuration}</p>
         </div>
