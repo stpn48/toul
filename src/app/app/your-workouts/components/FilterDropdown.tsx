f8545938fc3df88c4d319f4677d544542fbe34aa
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-17 19:46:28 +0200

    Create FilterDropdown.tsx

---
+++ src/app/app/your-workouts/components/FilterDropdown.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Filter } from "@/types/types";
+import { motion } from "framer-motion";
+import React, { useCallback, useEffect, useState } from "react";
+
+const filters = [
+  "a-z",
+  "z-a",
+  "times completed lowest to highest",
+  "times completed highest to lowest",
+  "last completed latest to oldest",
+  "last completed oldest to latest",
+  "created latest to oldest",
+  "created oldest to latest",
+];
+
+type Props = {
+  setFilter: React.Dispatch<React.SetStateAction<string>>;
+  closeDropdown: () => void;
+};
+
+export function FilterDropdown({ setFilter, closeDropdown }: Props) {
+  const handleChoiceClick = useCallback(
+    (filter: Filter) => {
+      setFilter(filter);
+      closeDropdown();
+    },
+    [setFilter, closeDropdown],
+  );
+
+  return (
+    <>
+      <div className="fixed inset-0 z-10 h-screen w-screen" onClick={closeDropdown} />
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="absolute top-5 z-20 flex flex-col gap-2 rounded-lg border border-main bg-secondary p-4 text-sm dark:border-dark-main dark:bg-dark-secondary"
+      >
+        {filters.map((filter) => (
+          <div key={filter}>
+            <button
+              onClick={() => handleChoiceClick(filter as Filter)}
+              className="whitespace-nowrap rounded-lg p-2 hover:bg-hover dark:hover:bg-dark-hover"
+            >
+              {filter}
+            </button>
+            <div className="h-px w-full bg-hover last:h-0 dark:bg-dark-hover"></div>
+          </div>
+        ))}
+      </motion.div>
+    </>
+  );
+}
