f1893ec49a3f6625fdfa457686cfc018b3214d5b
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-17 14:42:06 +0200

    Update ExerciseDetailsModal.tsx

--- src/app/app/completed-exercises/components/ExerciseDetailsModal.tsx
+++ src/app/app/completed-exercises/components/ExerciseDetailsModal.tsx
@@ -3,6 +3,7 @@
 import { Modal } from "@/components/Modal";
 import { ExerciseWithSets } from "@/types/types";
 import React from "react";
+import { ChartInformation } from "./ChartInformation";
 import { RepsChart } from "./RepsChart";
 
 type Props = {
@@ -12,14 +13,20 @@ type Props = {
 
 export function ExerciseDetailsModal({ exercise, closeModal }: Props) {
   return (
-    <Modal className="flex flex-col gap-8 p-4" closeModal={closeModal}>
+    <Modal className="flex flex-col gap-8 overflow-auto p-4 pr-6" closeModal={closeModal}>
       <div>
         <h1 className="text-xl font-bold">{exercise.name}</h1>
         <p className="text-secondary dark:text-dark-secondary">{exercise.description}</p>
       </div>
 
-      <h1 className="flex w-full justify-center">your progress</h1>
-      <RepsChart reps={exercise.repHistory} />
+      <div>
+        <div className="mb-4 flex w-full justify-between">
+          <h1>your progress</h1>
+
+          <ChartInformation />
+        </div>
+        <RepsChart reps={exercise.repHistory} />
+      </div>
       <div className="flex w-full flex-col items-center gap-4 text-sm text-secondary">
         <h2>last completed: {exercise.lastCompletedAt?.toLocaleDateString()}</h2>
         <h2>times completed: {exercise.timesCompleted}</h2>
