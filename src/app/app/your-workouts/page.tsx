4f7bbabdee29f83f9465d0522068990f03714fe0
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-18 22:21:25 +0200

    Update page.tsx

--- src/app/app/your-workouts/page.tsx
+++ src/app/app/your-workouts/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FilterDropdownButton } from "./components/FilterDropdownButton";
+import { Filters } from "./components/Filters";
 import { SearchBar } from "./components/SearchBar";
 import { WorkoutList } from "./components/WorkoutList";
 
@@ -7,10 +8,7 @@ export default async function YourWorkoutsPage() {
   return (
     <div className="flex h-full w-full flex-grow flex-col gap-8 text-main dark:text-dark-main">
       <h1 className="flex w-full justify-center text-2xl font-bold">your workouts</h1>
-      <div className="flex items-center gap-2">
-        <SearchBar />
-        <FilterDropdownButton />
-      </div>
+      <Filters />
       <WorkoutList />
     </div>
   );
