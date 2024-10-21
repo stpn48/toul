e4b06fa253e099371eab3a98fb7f5fcc4b0f4640
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-21 18:20:52 +0200

    Update loading.tsx

--- src/app/app/@workoutDetailsModal/(.)workout-details/[id]/loading.tsx
+++ src/app/app/@workoutDetailsModal/(.)workout-details/[id]/loading.tsx
@@ -6,8 +6,8 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <Modal className="flex flex-row" closeModal={() => {}}>
-      <section className="flex w-[30%] flex-col gap-6 rounded-l-lg bg-secondary p-6 dark:bg-dark-secondary">
+    <Modal className="flex flex-col lg:flex-row" closeModal={() => {}}>
+      <section className="flex w-full flex-col gap-6 rounded-l-lg bg-secondary p-6 dark:bg-dark-secondary lg:w-[30%]">
         <div className="h-[60px] w-full animate-pulse rounded-lg bg-stone-300 dark:bg-stone-900" />
         <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-300 dark:bg-stone-900" />
         <div className="h-[36px] w-full animate-pulse rounded-lg bg-stone-300 dark:bg-stone-900" />
